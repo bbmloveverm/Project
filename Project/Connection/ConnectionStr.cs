ba9f15ff814699e14aeee00355741d0eb6f07bd6
montree-ri <montree-ri@IT-27000208>
2020-07-06 16:00:00 +0700

    Class connect

---
+++ Project/Connection/ConnectionStr.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Connection
+{
+    class ConnectionStr
+    {
+
+        public static string strcon;
+        public static string UserfullnameLogin;
+
+        public static void connectsql()
+        {
+            strcon = "";
+        }
+
+
+        public static DataTable executsql(string sql)
+        {
+            SqlDataAdapter dtAdapter = default(SqlDataAdapter);
+            SqlConnection objConn = new SqlConnection();
+            DataTable dt = new DataTable();
+
+            try
+            {
+                objConn.ConnectionString = strcon;
+                objConn.Open();
+
+                dtAdapter = new SqlDataAdapter(sql,objConn);
+                dtAdapter.Fill(dt);
+                objConn.Close();
+                objConn = null;
+                return dt;
+            }
+            catch (Exception Error)
+            {
+                Console.Write(Error.ToString());
+                dt = null;
+
+            }
+
+            return dt;
+        }
+    }
+}
